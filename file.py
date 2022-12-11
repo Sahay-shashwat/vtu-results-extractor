6b04069ef86c0c9437d9f923d5020340bb1bb8ca
Rijuth Menon <rijuthm@gmail.com>
2022-12-11 14:56:52 +0530

    added func to write to csv file

--- file.py
+++ file.py
@@ -1,14 +1,29 @@
 import os
 from datetime import date
+import csv
 
 
 class File:
 
     def __init__(self, sem, reval, folderName):
         self.path = os.path.join(os.getcwd(), folderName)
-        if not reval:
-            self.name = f"{str(date.today())}-reg-{sem}"
-        else:
-            self.name = f"{str(date.today())}-reval-{sem}"
+        # Creating folder if not exists
+        if not os.path.isdir(os.path.join(os.getcwd(), folderName)):
+            os.makedirs(folderName)
 
-    def addData(self)
+        if not reval:
+            self.name = f"{str(date.today())}-reg-{sem}.csv"
+        else:
+            self.name = f"{str(date.today())}-reval-{sem}.csv"
+
+    def addData(self, data):
+        # Manipulating data from db to insert
+        usn = data[0][1]
+        name = data[0][2]
+        sem = data[0][3]
+        data = [ele for row in data for ele in row[4:-1]]
+        data.insert(0, usn)
+        data.insert(1, name)
+        data.insert(2, sem)
+        with open(os.path.join(self.path, self.name), 'a') as csvfile:
+            csvwriter = csv.writer(csvfile).writerow(data)
