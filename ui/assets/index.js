3fac668302f92a22bcbf6cc06efca37c465940f7
Rijuth Menon <rijuthm@gmail.com>
2022-12-11 22:54:12 +0530

    added function to extract USN's

--- ui/assets/index.js
+++ ui/assets/index.js
@@ -24,3 +24,33 @@ toastr.options = {
     "hideMethod": "fadeOut"
 }
 
+function extract()
+{
+    var resultLink = document.getElementById('link').value;
+    var usn = document.getElementById('usn').value;
+
+    if (!/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/.test(resultLink))
+    {
+        toastr["error"]("", "Please enter a valid link!");
+    }
+    else if (!/^\w+(,\w+)*$/.test(usn))
+    {
+        toastr["error"]("", "Please enter valid USN's!");
+    }
+    else
+    {
+        eel.extract(usn, resultLink)(function (ret)
+        {
+            console.log(ret);
+            if (ret.status)
+            {
+                toastr["success"]("", `${ret.len} USN's extracted!`);
+            }
+            else
+            {
+                toastr["error"]("", "Error occured while extracting!");
+            }
+        });
+    }
+
+}
