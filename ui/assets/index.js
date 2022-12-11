471eca2178c73acc6407f2a86e638b025093fc0e
Rijuth Menon <rijuthm@gmail.com>
2022-12-11 22:53:51 +0530

    added toastr

---
+++ ui/assets/index.js
@@ -0,0 +1,26 @@
+/*
+* Main JS file
+* Core JS functions for UI
+* @author: Rijuth Menon
+* @copyright: https://rijuthmenon.me
+*/
+
+// For Toastr
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "900",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1500",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+
