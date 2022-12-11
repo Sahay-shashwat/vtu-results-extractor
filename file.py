b54e168ad52b217b35aa80903e3fd0b7847919fc
Rijuth Menon <rijuthm@gmail.com>
2022-12-11 16:47:42 +0530

    fixed indexing since db structure was changed

--- file.py
+++ file.py
@@ -19,15 +19,15 @@ class File:
     def addData(self, data):
         # Manipulating data from db to insert
         try:
-            if len(data) > 3:
-                usn = data[0][1]
-                name = data[0][2]
-                sem = data[0][3]
+            if len(data) >= 1:
+                usn = data[0][0]
+                name = data[0][1]
+                sem = data[0][2]
                 data = [ele for row in data for ele in row[4:-1]]
                 data.insert(0, usn)
                 data.insert(1, name)
                 data.insert(2, sem)
-                with open(os.path.join(self.path, self.name), 'a') as csvfile:
-                    csv.writer(csvfile).writerow(data)
+                with open(os.path.join(self.path, self.name), 'a', newline='') as csvfile:
+                    csvwriter = csv.writer(csvfile).writerow(data)
         except:
             print("Error occured while writing data!")
