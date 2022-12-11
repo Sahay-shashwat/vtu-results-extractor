70d5cfa99d88bf34a995948cea98dbebaf82bebe
Rijuth Menon <rijuthm@gmail.com>
2022-12-11 17:19:53 +0530

    added enum class

---
+++ enums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class ExtractorEnums(Enum):
+    BASE_DOMAIN = 'https://results.vtu.ac.in'
+    INDEX_URL = 'JJEcbcs22/index.php'
+    RESULT_URL = 'JJEcbcs22/resultpage.php'
+    OUTPUT_FOLDER_NAME = 'outputs'
