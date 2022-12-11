0583e867f720575e5d281d97873008f443bef838
Rijuth Menon <rijuthm@gmail.com>
2022-12-11 19:36:27 +0530

    restructured Enums

--- enums.py
+++ enums.py
@@ -3,6 +3,6 @@ from enum import Enum
 
 class ExtractorEnums(Enum):
     BASE_DOMAIN = 'https://results.vtu.ac.in'
-    INDEX_URL = 'JJEcbcs22/index.php'
-    RESULT_URL = 'JJEcbcs22/resultpage.php'
+    INDEX_FILE = 'index.php'
+    RESULT_FILE = 'resultpage.php'
     OUTPUT_FOLDER_NAME = 'outputs'
