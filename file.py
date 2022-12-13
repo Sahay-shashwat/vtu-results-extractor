6c3765a030e5529b3180ae9e03479ca6accb0691
Rijuth Menon <rijuthm@gmail.com>
2022-12-13 20:40:36 +0530

    fixed file name format

--- file.py
+++ file.py
@@ -12,9 +12,9 @@ class File:
             os.makedirs(folderName)
 
         if not reval:
-            self.name = f"{str(date.today())}-reg-{sem}.csv"
+            self.name = f"reg-{sem}.csv"
         else:
-            self.name = f"{str(date.today())}-reval-{sem}.csv"
+            self.name = f"reval-{sem}.csv"
 
     def addData(self, data):
         # Manipulating data from db to insert
@@ -23,11 +23,14 @@ class File:
                 usn = data[0][0]
                 name = data[0][1]
                 sem = data[0][2]
+                announced = data[0][-2]
                 data = [ele for row in data for ele in row[4:-1]]
                 data.insert(0, usn)
                 data.insert(1, name)
                 data.insert(2, sem)
+                # Adding announced date to file
+                self.name = f"{str(date.today())}_{str(announced)}-{self.name}"
                 with open(os.path.join(self.path, self.name), 'a', newline='') as csvfile:
                     csvwriter = csv.writer(csvfile).writerow(data)
-        except:
-            print("Error occured while writing data!")
+        except Exception as e:
+            print("Error occured while writing data!", e)
